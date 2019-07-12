c0cc3aa60090746472b86c7ce28c445115b2371a
Corey Hickok <chickok@cynergydigital.com>
2019-07-12 04:40:29 -0400

    Fix for initial app load and when deleting (#30)

--- Moggles/ClientApp/AppSelection.vue
+++ Moggles/ClientApp/AppSelection.vue
@@ -10,64 +10,56 @@
 	import _ from 'lodash'
 
 	export default {
-        data() {
-            return { 
+		data() {
+			return {
 				applicationList: [],
 				selectedApps: []
 			}
-        },
-        methods: {
+		},
+		methods: {
 			changeApp() {
 				var app = _.find(this.applicationList, (a) => a.id == this.selectedApps[0])
-                if (app) {
+				if (app) {
 					Bus.$emit('app-changed', app)
 					this.$refs.appSelection.showDropdown = false
-                }
-            },
+				}
+			},
 			getApplications() {
 				axios.get('/api/applications')
 					.then((response) => {
 						this.applicationList = response.data
-                        if (!this.selectedAppId) {
+						if (!this.appIsSelected) {
 							if (response.data.length > 0) {
 								if (this.selectedApps.length == 0) {
 									this.selectedApps.push(response.data[0].id);
+									this.changeApp();
 								}
-                            }
+							}
 						}
-                    })
-                    .catch(error => { window.alert(error) })
-            },
-            refreshApps() {
-                axios.get('/api/applications')
-                    .then((response) => {
-                        this.applicationList = response.data;
-                        this.selectFirstApp();
-                    })
-                    .catch(error => { window.alert(error) })
-            },
-            selectFirstApp() {
-                this.selectedApps = [];
-                this.selectedApps.push(this.applicationList[0].id);
-
-                var app = this.applicationList[0];
-                if (app) {
-                    Bus.$emit('app-changed', app)
-                }
-            }
-
-        },
+					})
+					.catch(error => { window.alert(error) })
+			},
+			refreshApps() {
+				this.selectedApps = []
+				this.getApplications()
+			},
+		},
 		created() {
 			this.getApplications()
 			Bus.$on("new-app-added", () => {
-                this.getApplications();      
+				this.getApplications();
 			});
 			Bus.$on("reload-application-toggles", () => {
 				this.changeApp();
-            });
-            Bus.$on("refresh-apps", () => {               
-                this.refreshApps();   
-            });
+			});
+			Bus.$on("refresh-apps", () => {
+				this.refreshApps();
+			});
+		},
+		computed: {
+			appIsSelected() {
+				return this.selectedApps.length > 0;
+			}
 		}
     }
 </script>
