4de37af34b2916e471dca819af73c29c58e506a3
Ramona Toadere <ramona_toadere@yahoo.com>
2019-07-23 17:06:27 +0300

    Added new smoke tests

---
+++ MogglesEndToEnd/MogglesEndToEndTests/SmokeTests/EditAndDeleteEnvironment.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MogglesEndToEndTests.TestFramework;
+
+namespace MogglesEndToEndTests.SmokeTests
+{
+    [TestClass]
+    public class EditAndDeleteEnvironment
+    {
+        [TestInitialize]
+        public void Before()
+        {
+            ChromeDriverUtils.KillChromeDriverProcesses();
+            Browser.CreateNew(Constants.HeadlessModeEnabled);
+        }
+
+        [TestMethod]
+        [TestCategory("EditANewEnvironment")]
+        [TestCategory("SmokeTests")]
+
+        public void EditAndDeleteEnvironment_TheEnvironmentIsUpdatedAndAfterThatDeleted()
+        {
+            //act
+            Pages.FeatureTogglesPage.Navigate();
+            Pages.FeatureTogglesPage.AddNewApplication(Constants.NewApplicationName, Constants.FirstEnvName);
+            Pages.FeatureTogglesPage.SelectASpecificApplication(Constants.NewApplicationName);
+            Pages.FeatureTogglesPage.AddNewEnvironment(Constants.SecondEnvName);
+            Pages.FeatureTogglesPage.EditEnvironment(Constants.SecondEnvName);
+            Pages.FeatureTogglesPage.ChangeEnvironmentName(Constants.EditedSecondEnvName);
+
+            //assert
+            Pages.FeatureTogglesPage.EnvironmentNameExist(Constants.EditedSecondEnvName).Should().BeTrue();
+
+            //act
+            Pages.FeatureTogglesPage.DeleteEnvironment(Constants.EditedSecondEnvName);
+
+            //assert
+            Pages.FeatureTogglesPage.EnvironmentNameExist(Constants.EditedSecondEnvName).Should().BeFalse();
+        }
+
+        [TestCleanup]
+        public void After()
+        {
+            Pages.FeatureTogglesPage.DeleteApplication();
+            Browser.Close();
+            ChromeDriverUtils.KillChromeDriverProcesses();
+        }
+    }
+}
