71ac3bdc7530c51f6a59dd68d310c7a66c3d9392
Liviu Mandras <liviu@mandras-iura.ro>
2019-10-31 13:28:02 +0200

    added runsettings file

---
+++ Moggles.EndToEndTests/TestFramework/InitTestClass.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MogglesEndToEndTests.TestFramework;
+
+namespace Moggles.EndToEndTests.TestFramework
+{
+    [TestClass]
+    public class InitTestClass
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            if (!context.Properties.Contains("webAppUrl"))
+                return;
+
+            var appUrl = context.Properties["webAppUrl"]?.ToString();
+
+            if (!string.IsNullOrEmpty(appUrl))
+                Constants.BaseUrl = appUrl;
+        }
+
+        [TestMethod]
+        public void WramupTest()
+        {
+
+        }
+    }
+}
