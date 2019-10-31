004ff72b7de1258a9c45bbf4af48a921492ef73a
Ramona-Alexandra Toadere <ramona_toadere@yahoo.com>
2019-10-31 16:15:34 +0200

    Config changes (#60)
    * Config changes
    
    * More changes

--- Moggles.EndToEndTests/TestFramework/Constants.cs
+++ Moggles.EndToEndTests/TestFramework/Constants.cs
@@ -1,10 +1,8 @@
-﻿using Moggles.EndToEndTests.TestFramework;
-
-namespace MogglesEndToEndTests.TestFramework
+﻿namespace MogglesEndToEndTests.TestFramework
 {
     public class Constants
     {
-        public static string BaseUrl = AppSettingsReader.GetApplicationConfiguration().BaseUrl;
+        public static string BaseUrl = "http://moggles.northernsafety-dev.com/";
 
         public static string FeatureToggleName = "SmokeTestsFeatureToggle";
         public static string SmokeTestsApplication = "SmokeTests";
