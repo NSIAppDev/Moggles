1b31907258ce6b4e353179003287bba4e2d7f1a7
Patricia Mazere <patriciamazere@gmail.com>
2019-11-05 10:50:20 -0500

    Add unit tests

--- Moggles.UnitTests/Helpers/FeatureToggleHelper.cs
+++ Moggles.UnitTests/Helpers/FeatureToggleHelper.cs
@@ -10,7 +10,7 @@ namespace Moggles.UnitTests.Helpers
 
         public static FeatureToggleStatusData GetFeatureToggleStatus(Application app, string toggleName, string environment)
         {
-            var toggle = app.FeatureToggles.FirstOrDefault(f => string.Compare(f.ToggleName, toggleName, StringComparison.OrdinalIgnoreCase) == 0);
+            var toggle = GetToggle(app, toggleName);
             var featureStatus = toggle.GetFeatureToggleStatusForEnv(environment);
 
             return new FeatureToggleStatusData
@@ -19,6 +19,25 @@ namespace Moggles.UnitTests.Helpers
                 Enabled = featureStatus.Enabled,
                 UpdatedBy = featureStatus.UpdatedbyUser
             };
+        }
+
+        public static void UpdateFeatureToggleDeployedStatus(Application app, string toggleName, string environment, bool deployedStatus)
+        {
+            var toggle = GetToggle(app, toggleName);
+            var featureStatus = toggle.GetFeatureToggleStatusForEnv(environment);
+            featureStatus.IsDeployed = deployedStatus;
+        }
+
+        public static bool GetFeatureToggleDeployedStatus(Application app, string toggleName, string environment)
+        {
+            var toggle = GetToggle(app, toggleName);
+            var featureStatus = toggle.GetFeatureToggleStatusForEnv(environment);
+            return featureStatus.IsDeployed;
+        }
+
+        private static FeatureToggle GetToggle(Application app, string toggleName)
+        {
+            return app.FeatureToggles.FirstOrDefault(f => string.Compare(f.ToggleName, toggleName, StringComparison.OrdinalIgnoreCase) == 0);
         }
     }
 }
