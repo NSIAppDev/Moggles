d75e93053dc36e868fa2b5057f4e76d63e9bba35
patriciamazere <patriciamazere@gmail.com>
2019-11-05 12:00:59 -0500

    Fix show deployed status (#62)
    * Fix show deployed status
    
    * Add unit tests
    
    * Update class name

---
+++ Moggles.UnitTests/Helpers/FeatureToggleTestHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Moggles.Domain;
+using Moggles.Domain.DTO;
+
+namespace Moggles.UnitTests.Helpers
+{
+    public static class FeatureToggleTestHelper
+    {
+
+        public static FeatureToggleStatusData GetFeatureToggleStatus(Application app, string toggleName, string environment)
+        {
+            var toggle = GetToggle(app, toggleName);
+            var featureStatus = toggle.GetFeatureToggleStatusForEnv(environment);
+
+            return new FeatureToggleStatusData
+            {
+                EnvironmentName = featureStatus.EnvironmentName,
+                Enabled = featureStatus.Enabled,
+                UpdatedBy = featureStatus.UpdatedbyUser
+            };
+        }
+
+        public static void UpdateFeatureToggleDeployedStatus(Application app, string toggleName, string environment, bool deployedStatus)
+        {
+            var toggle = GetToggle(app, toggleName);
+            var featureStatus = toggle.GetFeatureToggleStatusForEnv(environment);
+            featureStatus.IsDeployed = deployedStatus;
+        }
+
+        public static bool GetFeatureToggleDeployedStatus(Application app, string toggleName, string environment)
+        {
+            var toggle = GetToggle(app, toggleName);
+            var featureStatus = toggle.GetFeatureToggleStatusForEnv(environment);
+            return featureStatus.IsDeployed;
+        }
+
+        private static FeatureToggle GetToggle(Application app, string toggleName)
+        {
+            return app.FeatureToggles.FirstOrDefault(f => string.Compare(f.ToggleName, toggleName, StringComparison.OrdinalIgnoreCase) == 0);
+        }
+    }
+}
---
+++ Moggles.UnitTests/ShowDeployedTogglesTests/ShowDeployedTogglesTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moggles.Consumers;
+using Moggles.Domain;
+using Moggles.UnitTests.Helpers;
+using MassTransit.TestFramework;
+using MogglesContracts;
+
+namespace Moggles.UnitTests.ShowDeployedTogglesTests
+{
+    [TestClass]
+    public class ShowDeployedTogglesTests
+    {
+        private IRepository<Application> _appRepository;
+        private FeatureToggleDeployStatusConsumer _sut;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _appRepository = new InMemoryApplicationRepository();
+        }
+
+        [TestMethod]
+        public async Task FeatureToggleShowDeployedEnvironmentIsUpdatedCorrectly()
+        {
+            //arrange
+            var app = Application.Create("TestApp", "DEV", false);
+            app.AddFeatureToggle("TestToggle-1", "TestNotes-1");
+            app.AddFeatureToggle("TestToggle-2", "TestNotes-2");
+
+            await _appRepository.AddAsync(app);
+
+            FeatureToggleTestHelper.UpdateFeatureToggleDeployedStatus(app, "TestToggle-1", "DEV", true);
+
+            var context = new TestConsumeContext<RegisteredTogglesUpdate>(new RegisteredTogglesUpdate
+            {
+                Environment = "dev",
+                AppName = "testapp",
+                FeatureToggles = new []{ "TestToggle-2" }
+            });
+
+            //act
+            _sut = new FeatureToggleDeployStatusConsumer(_appRepository);
+            await _sut.Consume(context);
+
+            //assert
+            var updatedApp = await _appRepository.FindByIdAsync(app.Id);
+            FeatureToggleTestHelper.GetFeatureToggleDeployedStatus(updatedApp, "TestToggle-1", "DEV").Should().BeFalse();
+            FeatureToggleTestHelper.GetFeatureToggleDeployedStatus(updatedApp, "TestToggle-2", "DEV").Should().BeTrue();
+        }
+    }
+}
