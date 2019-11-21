35f320712ad9a6ff990d0084578ebe0592203d83
Ramona-Alexandra Toadere <ramona_toadere@yahoo.com>
2019-11-21 17:23:25 +0200

    Environment refresh test (#71)
    * Environment Refresh Test
    
    * More changes
    
    * Changes after code review

---
+++ Moggles.EndToEndTests/SmokeTests/RefreshEnvironment.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moggles.EndToEndTests.TestFramework;
+using MogglesEndToEndTests.TestFramework;
+using NSTestFrameworkDotNetCoreUI.Helpers;
+
+namespace Moggles.EndToEndTests.SmokeTests
+{
+    [TestClass]
+    public class RefreshEnvironment : BaseTest
+    {
+        [TestMethod]
+        [TestCategory("RefreshEnvironment")]
+        [TestCategory("SmokeTests")]
+
+        public void UpdateEnvironment_RefreshIsTriggered()
+        {
+            //act
+            Browser.Goto(Constants.BaseUrl);
+            Pages.FeatureTogglesPage.SelectASpecificApplication(Constants.SmokeTestsApplication);
+            Pages.FeatureTogglesPage.AddFeatureToggle(Constants.FeatureToggleName);
+            Pages.FeatureTogglesPage.EditFeatureToggle(Constants.FeatureToggleName);
+            Pages.FeatureTogglesPage.UpdateDevEnvironment();
+            Pages.FeatureTogglesPage.RefreshEnvironment();
+
+            //assert
+            Pages.FeatureTogglesPage.IsRefreshedEnvironmentMessageIsDisplayed().Should().BeTrue();
+        }
+
+        [TestCleanup]
+        public override void After()
+        {
+            Pages.FeatureTogglesPage.DeleteFeatureToggle(Constants.FeatureToggleName);
+            base.After();
+        }
+    }
+}
