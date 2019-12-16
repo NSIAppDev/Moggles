3e8d062d440e427819797a8cef76f3144d3d78da
Ramona Toadere <ramona_toadere@yahoo.com>
2019-12-16 10:07:49 +0200

    Historical data and feature toggle scheduler acceptance tests

---
+++ Moggles.EndToEndTests/SmokeTests/FeatureToggleHistory.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moggles.EndToEndTests.TestFramework;
+using MogglesEndToEndTests.TestFramework;
+using NSTestFrameworkDotNetCoreUI.Helpers;
+using Moggles.Models;
+using System;
+using NSTestFrameworkDotNetCoreApi.RestSharp;
+using NSTestFrameworkDotNetCoreApi.RestSharp.model;
+
+
+namespace Moggles.EndToEndTests.SmokeTests
+{
+    [TestClass]
+    public class FeatureToggleHistory : BaseTest
+    {
+        [TestInitialize]
+        public override void Before()
+        {
+           var featureToggle = new FeatureToggleAddModel
+            {
+                ApplicationId = "1e6ba677-2af3-446d-b747-476542fca042",
+                FeatureToggleName = Constants.FeatureToggleName,
+                Notes = "",
+                IsPermanent = false
+            };
+           Test.AddFeatureFlagg(featureToggle);
+            base.Before();
+        }
+
+
+        [TestMethod]
+        [TestCategory("SmokeTests")]
+        public void UpdateFeatureToggle_TheHistoryIsUpdated()
+        {
+            //act 
+            Browser.Goto(Constants.BaseUrl);
+            Pages.FeatureTogglesPage.SelectASpecificApplication(Constants.SmokeTestsApplication);
+            Pages.FeatureTogglesPage.EditFeatureToggle(Constants.FeatureToggleName);
+
+        }
+    }
+}
---
+++ Moggles.EndToEndTests/SmokeTests/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using MogglesEndToEndTests.TestFramework;
+using RestSharp.Authenticators;
+
+namespace Moggles.EndToEndTests.SmokeTests
+{
+    public static class Test
+    {
+        private static RestClient Client => new RestClient(Constants.BaseUrl);
+        public  static void AddFeatureFlagg(FeatureToggleAddModel featureToggleAddModel)
+        {
+            var request = GetRequest("api/featuretoggles/addFeatureToggle", featureToggleAddModel, Method.POST);
+            request.AddHeader("Content-Type", "application/json;charset=UTF-8");
+            Client.Authenticator = new NtlmAuthenticator("rtoadere", "RAMTOASafety2");
+            Client.Execute(request);
+        }
+
+        public static RestRequest GetRequest(string resource, object bodyParameter, Method method = Method.GET)
+        {
+            var request = new RestRequest(resource, method) { RequestFormat = DataFormat.Json };
+            request.AddJsonBody(bodyParameter);
+            return request;
+        }
+    }
+
+    public class FeatureToggleAddModel
+    {
+        public string ApplicationId { get; set; }
+        public string FeatureToggleName { get; set; }
+        public string Notes { get; set; }
+        public bool IsPermanent { get; set; }
+    }
+}
