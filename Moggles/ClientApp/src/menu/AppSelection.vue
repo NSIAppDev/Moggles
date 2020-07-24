e10ff4d7358508bea6642ee26b23db68b3ddff36
patriciamazere <patriciamazere@gmail.com>
2020-07-24 15:05:44 +0300

    Fix Bug 62768 (#159)
    * Fix bug 62768
    
    * Update lodash version

--- Moggles/ClientApp/src/menu/AppSelection.vue
+++ Moggles/ClientApp/src/menu/AppSelection.vue
@@ -1,6 +1,6 @@
 ﻿<template>
-  <div id="app-sel"> 
-    <multi-select id="selectedApp" ref="appSelection" v-model="selectedApp"
+  <div id="app-sel">
+    <multi-select id="selectedApp" ref="appSelection" v-model="selectedAppId"
                   :limit="1"
                   :options="applications" :value-key="'id'" :label-key="'appName'"
                   :selected-icon="'fas fa-check'" class="padding-left-10" @change="changeApp" />
@@ -8,88 +8,88 @@
 </template>
 
 <script>
-	import { Bus } from '../common/event-bus';
-	import { events } from '../common/events';
-	import axios from 'axios';
-	import _ from 'lodash';
+    import { Bus } from '../common/event-bus';
+    import { events } from '../common/events';
+    import axios from 'axios';
+    import _ from 'lodash';
 
-	export default {
-		data() {
-			return {
-				applications: [],
-				selectedApp: []
-			}
-		},
-		computed: {
-			appIsSelected() {
-				return this.selectedApp.length > 0;
-			}
-		},
-		created() {
-			this.getApplications();
+    export default {
+        data() {
+            return {
+                applications: [],
+                selectedAppId: [],
+                selectedAppIdLocalStorageKey: 'selectedAppId'
+            }
+        },
+        created() {
+            this.getApplications();
 
-			Bus.$on(events.newApplicationAdded, () => {
-				this.getApplications();
-			});
+            Bus.$on(events.newApplicationAdded, () => {
+                this.getApplications();
+            });
 
-			Bus.$on(events.applicationEdited, () => {
-				this.getApplications();
-			});
+            Bus.$on(events.applicationEdited, () => {
+                this.getApplications();
+            });
 
-			Bus.$on(events.reloadApplicationToggles, () => {
-				this.changeApp();
-			});
+            Bus.$on(events.reloadApplicationToggles, () => {
+                this.changeApp();
+            });
 
-			Bus.$on(events.refreshApplications, () => {
-				this.refreshApps();
-			});
-		},
-		methods: {
-            changeApp() {
-                var app =_.find(this.applications, (a) => a.id == this.selectedApp[0]);
-                if (app) {
-					Bus.$emit('block-ui')
-                    Bus.$emit(events.applicationChanged, app);
-                    localStorage.setItem('selectedApp', app.id);
-					this.$refs.appSelection.showDropdown = false;
-				}
-			},
+            Bus.$on(events.refreshApplications, () => {
+                this.refreshApps();
+            });
+        },
+        methods: {
             getApplications() {
-				axios.get('/api/applications')
-					.then((response) => {
-						this.applications = response.data
-						if (!this.appIsSelected) {
-							if (response.data.length > 0) {
-                                if (this.selectedApp.length == 0) {
-                                    if (localStorage.getItem('selectedApp') === null || !this.existsStoredApp()) {
-                                        this.selectedApp.push(response.data[0].id);
-                                    }
-                                    else {
-                                        this.selectedApp.push(localStorage.getItem('selectedApp'));
-                                    }
-                                    this.changeApp();
-								}
-							}
-						}
+                axios.get('/api/applications')
+                    .then((response) => {
+                        this.applications = response.data;
+
+                        if (!this.isApplicationSelected() && this.applications.length > 0) {
+                            var localStorageSelectedApp = this.getSelectedAppFromLocalStorage();
+                            if (localStorageSelectedApp != null) {
+                                this.selectedAppId = [localStorageSelectedApp.id];
+                            }
+                            else {
+                                this.selectedAppId = [this.applications[0].id];
+                            }
+
+                            this.setNewApplicationSelection();
+                        }
+
                     }).catch(error => {
                         Bus.$emit(events.showErrorAlertModal, { 'error': error });
                     });
             },
-			refreshApps() {
-				this.selectedApp = [];
-				this.getApplications();
+            changeApp() {
+                if (!this.isApplicationSelected()) {
+                    this.getApplications();
+                } else {
+                    this.setNewApplicationSelection();
+                }
             },
-            getAllAplications() {
-                axios.get('/api/applications')
-                    .then((response) => {
-                        this.application = response.data;
-                    })
-                    .catch(error => Bus.$emit(events.showErrorAlertModal, { 'error': error }));
+            setNewApplicationSelection() {
+                let selectedApp = _.find(this.applications, (application) => application.id == this.selectedAppId[0]);
+                Bus.$emit('block-ui')
+                Bus.$emit(events.applicationChanged, selectedApp);
+                localStorage.setItem(this.selectedAppIdLocalStorageKey, this.selectedAppId[0]);
+                this.$refs.appSelection.showDropdown = false;
             },
-            existsStoredApp() {
-                var app = _.find(this.applications, (a) => a.id == localStorage.getItem('selectedApp'));
-                return app != null 
+            refreshApps() {
+                this.selectedAppId = [];
+                this.getApplications();
+            },
+            getApplication(applicationId) {
+                return _.find(this.applications, (application) => application.id == applicationId);
+            },
+            getSelectedAppFromLocalStorage() {
+                var localStorageSelectedAppId = localStorage.getItem(this.selectedAppIdLocalStorageKey);
+                return this.getApplication(localStorageSelectedAppId);
+            },
+            isApplicationSelected() {
+                return this.selectedAppId != null && this.selectedAppId.length > 0;
             }
-		}
+        }
     }
 </script>
