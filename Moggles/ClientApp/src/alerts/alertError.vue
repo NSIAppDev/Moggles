57d6547dfabb1f3677eaa63b16d4b323240ca03d
Patricia Mazere <patriciamazere@gmail.com>
2020-07-24 12:11:39 +0300

    Fix bug 62768

--- Moggles/ClientApp/src/alerts/alertError.vue
+++ Moggles/ClientApp/src/alerts/alertError.vue
@@ -1,39 +1,34 @@
 ﻿<template>    
-    <div>
-        <h4>{{getMessage}}</h4>
-        <div v-if="getTechnicalDetails">
-            <button id="viewTechnicalDetailsBtn" class="btn btn-default errorDetailsBtn" @click="toggleDetails">
-                {{getDetailsButtonName}}
-            </button>
-            <div v-if="showTechnicalDetails">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="errorDetailsBox"> 
-                            {{getTechnicalDetails}}
-                        </div>                       
-                    </div>
-                </div>            
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12 text-right">
-                <button id="closeErrorAlertBtn" class="btn btn-primary" @click="cancel">
-                    Close
-                </button>
-            </div>
-        </div>
+  <div>
+    <h4>{{ getMessage }}</h4>
+    <div v-if="getTechnicalDetails">
+      <button id="viewTechnicalDetailsBtn" class="btn btn-default errorDetailsBtn" @click="toggleDetails">
+        {{ getDetailsButtonName }}
+      </button>
+      <div v-if="showTechnicalDetails">
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <div class="errorDetailsBox"> 
+              {{ getTechnicalDetails }}
+            </div>                       
+          </div>
+        </div>            
+      </div>
     </div>
+    <div class="row">
+      <div class="col-sm-12 text-right">
+        <button id="closeErrorAlertBtn" class="btn btn-primary" @click="cancel">
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
     import { Bus } from '../common/event-bus'
     import { events } from '../common/events';
 
     export default {
-        data() {
-            return {
-                showTechnicalDetails: false
-            }
-        },
         props: {
             error: {
                 type: Error,
@@ -42,6 +37,22 @@
             customErrorMessage: {
                 type: String,
                 default: null
+            }
+        },
+        data() {
+            return {
+                showTechnicalDetails: false
+            }
+        },
+        computed: {
+            getMessage() {
+                return this.customErrorMessage ? this.customErrorMessage : this.error.response.status + ' - ' + this.error.response.statusText;
+            },
+            getTechnicalDetails() {
+                return this.error ? this.error.response.data : null;
+            },
+            getDetailsButtonName() {
+                return this.showTechnicalDetails ? "Hide Technical Details" : "View Technical Details";
             }
         },
          created() {
@@ -58,17 +69,6 @@
             },
             reset() {
                 this.showTechnicalDetails = false;
-            }
-        },
-        computed: {
-            getMessage() {
-                return this.customErrorMessage ? this.customErrorMessage : this.error.response.status + ' - ' + this.error.response.statusText;
-            },
-            getTechnicalDetails() {
-                return this.error ? this.error.response.data : null;
-            },
-            getDetailsButtonName() {
-                return this.showTechnicalDetails ? "Hide Technical Details" : "View Technical Details";
             }
         }
     }
