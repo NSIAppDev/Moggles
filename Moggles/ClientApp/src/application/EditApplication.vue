3c029d96807d0bb5ad5c8a9bd694b0dc384756ca
Nestor Venegas <Nestor.Venegas@wurthindustry.com>
2025-06-16 09:44:30 -0500

    fixed lint issues on EditApplication

--- Moggles/ClientApp/src/application/EditApplication.vue
+++ Moggles/ClientApp/src/application/EditApplication.vue
@@ -1,55 +1,56 @@
 <template>
-    <div>
-        <div class="form-horizontal">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div v-for="error in editAppErrors" :key="error" class="text-danger margin-left-15">
-                        {{ error }}
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-4 control-label" for="appName">Application name:</label>
-                    <div class="col-sm-7">
-                        <input id="editApplicationNameInput" v-model="appName" type="text"
-                               class="form-control" name="appName">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-4 control-label" for="assignedTo">Assigned to:</label>
-                    <div class="col-sm-7">
-                        <select id="assignedToDropdown" v-model="assignedTo" class="form-control" name="assignedTo">
-                            <option v-for="option in assignedToOptions" :key="option" :value="option">
-                                {{ option }}
-                            </option>
-                        </select>
-                    </div>
-                </div>
-                <div class="clearfix">
-                    <div v-if="!application.isDeleted" class="col-sm-6">
-                        <button id="deleteApplicationBtn" type="button" class="btn btn-danger"
-                                @click="showDeleteConfirmationMessage">
-                            Delete
-                        </button>
-                    </div>
-                    <div v-else class="col-sm-6">
-                        <button id="reactivateApplicationBtn" type="button" class="btn btn-primary"
-                                @click="updateApp(true)">
-                            Reactivate
-                        </button>
-                    </div>
-                    <div class="col-sm-6 text-right">
-                        <button type="button" class="btn btn-default" @click="cancel">
-                            Cancel
-                        </button>
-                        <button id="saveEditApplicationBtn" type="button" class="btn btn-primary"
-                                @click="updateApp(false)">
-                            Save
-                        </button>
-                    </div>
-                </div>
-            </div>
+  <div>
+    <div class="form-horizontal">
+      <div class="row">
+        <div class="col-sm-12">
+          <div v-for="error in editAppErrors" :key="error" class="text-danger margin-left-15">
+            {{ error }}
+          </div>
         </div>
+        <div class="form-group">
+          <label class="col-sm-4 control-label" for="appName">Application name:</label>
+          <div class="col-sm-7">
+            <input id="editApplicationNameInput" v-model="appName" type="text" 
+                   class="form-control" name="appName">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="col-sm-4 control-label" for="assignedTo">Assigned to:</label>
+          <div class="col-sm-7">
+            <select id="assignedToDropdown" v-model="assignedTo" class="form-control" 
+                    name="assignedTo">
+              <option v-for="option in assignedToOptions" :key="option" :value="option">
+                {{ option }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="clearfix">
+          <div v-if="!application.isDeleted" class="col-sm-6">
+            <button id="deleteApplicationBtn" type="button" class="btn btn-danger" 
+                    @click="showDeleteConfirmationMessage">
+              Delete
+            </button>
+          </div>
+          <div v-else class="col-sm-6">
+            <button id="reactivateApplicationBtn" type="button" class="btn btn-primary" 
+                    @click="updateApp(true)">
+              Reactivate
+            </button>
+          </div>
+          <div class="col-sm-6 text-right">
+            <button type="button" class="btn btn-default" @click="cancel">
+              Cancel
+            </button>
+            <button id="saveEditApplicationBtn" type="button" class="btn btn-primary" 
+                    @click="updateApp(false)">
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
